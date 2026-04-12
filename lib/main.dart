290922738ff3d3ea81613f7587b1d634c933c2cc
hiofadlikaakbar <hioakbar1@gmail.com>
2026-04-12 07:58:36 +0700

    menambah settingan web app

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,16 @@ import 'features/auth/login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyCQa8o5lQc8Izf1lPwxwcAkbI9Yt7Qwwro",
+      authDomain: "marketplace-uts.firebaseapp.com",
+      projectId: "marketplace-uts",
+      storageBucket: "marketplace-uts.firebasestorage.app",
+      messagingSenderId: "608464027335",
+      appId: "1:608464027335:web:6b8f04fbb00fdbaeb9c8e2",
+    ),
+  );
   runApp(const MyApp());
 }
 
