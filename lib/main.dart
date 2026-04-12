4dee8337096ee866981961858d32edfb5ac15c30
hiofadlikaakbar <hioakbar1@gmail.com>
2026-04-12 16:10:29 +0700

    setting firebase messaging di main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'features/auth/login_page.dart';
 import 'features/auth/signup_page.dart';
 import 'features/splash/splash_screen.dart';
 import 'features/home/home_page.dart';
+
+/// 🔥 WAJIB: background handler harus top-level + entry point
+@pragma('vm:entry-point')
+Future<void> firebaseBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+
+  print("Background notif: ${message.notification?.title}");
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,6 +26,7 @@ void main() async {
       appId: "1:608464027335:web:6b8f04fbb00fdbaeb9c8e2",
     ),
   );
+  FirebaseMessaging.onBackgroundMessage(firebaseBackgroundHandler);
   runApp(const MyApp());
 }
 
