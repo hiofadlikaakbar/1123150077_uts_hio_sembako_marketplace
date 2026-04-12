904daa4dda030116f37d0a1c642424b33c0137ec
hiofadlikaakbar <hioakbar1@gmail.com>
2026-04-12 07:36:25 +0700

    mulai ngoding login_page

---
+++ lib/features/auth/login_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
