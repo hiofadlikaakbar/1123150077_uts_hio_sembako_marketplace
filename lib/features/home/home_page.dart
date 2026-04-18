2771435fa6ff7681a86a7eab0178804f03d603af
hiofadlikaakbar <hioakbar1@gmail.com>
2026-04-18 12:27:36 +0700

    revisi itembuilder di homepage

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../../services/notification_services.dart';
+import '../../models/product_models.dart';
+import 'package:provider/provider.dart';
+import '../providers/cart_provider.dart';
 import '../../widgets/product_card.dart';
 
 class HomePage extends StatefulWidget {
@@ -112,15 +115,33 @@ class _HomePageState extends State<HomePage> {
                     itemBuilder: (context, index) {
                       final item = products[index];
 
+                      final product = Product.fromFirestore(
+                        item.data() as Map<String, dynamic>,
+                        item.id,
+                      );
+
                       return ProductCard(
-                        docId: item.id,
-                        image: item['image'],
-                        name: item['name'],
-                        price: "Rp ${item['price']}",
-                        category: item['category'],
-                        stock: item['stock'],
-                        rating: (item['rating'] ?? 0).toDouble(),
-                        onTap: () {},
+                        docId: product.id,
+                        image: product.image,
+                        name: product.name,
+                        price: "Rp ${product.price}",
+                        category: product.category,
+                        stock: product.stock,
+                        rating: product.rating,
+                        onTap: () {
+                          Provider.of<CartProvider>(
+                            context,
+                            listen: false,
+                          ).addToCart(product);
+
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(
+                                "${product.name} ditambahkan ke cart",
+                              ),
+                            ),
+                          );
+                        },
                       );
                     },
                   );
