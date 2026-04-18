89f0f5fa51857fc61ae806bc89aa3b75d920434c
hiofadlikaakbar <hioakbar1@gmail.com>
2026-04-18 11:58:07 +0700

    membuat cart provider

---
+++ lib/features/providers/cart_provider.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CartProvider extends ChangeNotifier {
+  final List<Map<String, dynamic>> _items = [];
+
+  List<Map<String, dynamic>> get items => _items;
+
+  void addToCart(Map<String, dynamic> product) {
+    _items.add(product);
+    notifyListeners();
+  }
+}
